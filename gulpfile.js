03224d4edea5d4aa2072507e1d26c2fbf5fa6bc7
Andrey Makarenko <baksckua@gmail.net>
2023-02-14 22:22:59 +0200

    added cotegories

--- gulpfile.js
+++ gulpfile.js
@@ -33,9 +33,10 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
-  .pipe(concat('main.min.js'))
+    .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
     .pipe(browserSync.stream())
